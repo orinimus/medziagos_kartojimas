336a8ca61ea8730ae4d49cd7e3db43ee106489fb
Aurimas <aurim@DESKTOP-JO69DC8>
2021-04-28 19:51:05 +0300

    balta

--- Spalva.cs
+++ Spalva.cs
@@ -6,16 +6,16 @@ namespace medziagos_kartojimas
 {
     public class Spalva : ISpalva
     {
-        public Spalva(byte red, byte green, byte blue)
+        public Spalva(byte red=0, byte green=0, byte blue=0)
         {
             Red = red;
             Green = green;
             Blue = blue;
         }
 
-        public byte Red { get; set; }
-        public byte Green { get; set; }
-        public byte Blue { get; set; }
+        public byte Red { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public byte Green { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public byte Blue { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
 
     }
